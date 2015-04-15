1d70776b6a0f9e21eeda7e3e5e27c59d15f250d3
Ryan <rymeades@gmail.com>
2015-04-15 18:02:15 -0400

    StuffFromBen
    Still no idea what I'm doing

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'core/init.php';?>
+<?php include 'includes/overall/over_header.php';?>
+<!-- Default panel contents -->
+<?php include 'includes/overall/container.php';?>
+
+
+
+<?php output_errors($errors); ?>
+<?php include 'includes/overall/over_footer.php';?>
